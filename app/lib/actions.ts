ace1b4c15c1e3ad254283caad89d6bfa3e04d32e
WEIJIE <1132842293@qq.com>
2024-05-07 17:47:52 +0800

    not-found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,7 +21,6 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
  
@@ -65,7 +64,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
